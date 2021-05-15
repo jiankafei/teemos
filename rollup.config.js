281783430325875919a4b9bf88a14c8d6c72ca67
zr <Wll001992Yx@163.com>
2021-05-15 21:19:52 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import replace from '@rollup/plugin-replace';
+
+const WATCH = process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/burypoint.js',
+      name: 'barypoint',
+      format: 'iife',
+      sourcemap: WATCH,
+      strict: true,
+    },
+    {
+      file: 'dist/burypoint.es.js',
+      name: 'barypoint',
+      format: 'es',
+      sourcemap: WATCH,
+      strict: true,
+    },
+  ],
+  plugins: [
+    commonjs(),
+    resolve(),
+    json(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+    replace({
+      preventAssignment: true,
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    }),
+    !WATCH && terser(),
+  ],
+  watch: {
+    chokidar: true,
+    include: 'src/**',
+    exclude: 'node_modules/**',
+  },
+};
