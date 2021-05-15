4358538e747cbdb9aafe9d34dfa8f418e0aaa09b
zr <Wll001992Yx@163.com>
2021-05-15 21:23:38 +0800

    init

--- example/exp-vue/vue.config.js
+++ example/exp-vue/vue.config.js
@@ -1,19 +1,3 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const path = require('path');
-
 // const isDev = process.env.NODE_ENV === 'development';
 
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, '../../dist/index.es.js'),
-            to: path.resolve(__dirname, 'src/bp.es.js'),
-          },
-        ],
-      }),
-    ],
-  },
-};
+module.exports = {};
