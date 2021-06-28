2c0202cb31e97c95357df9942dc0a75a6d690b91
zr <Wll001992Yx@163.com>
2021-06-28 21:43:36 +0800

    opt req

--- src/catch/req.js
+++ src/catch/req.js
@@ -1,6 +1,4 @@
 // 覆写 XMLHttpRequest
-
-// 方案1
 window.XMLHttpRequest = class XMLHttpRequest extends window.XMLHttpRequest {
   constructor(...rest) {
     super(rest);
@@ -14,24 +12,6 @@ window.XMLHttpRequest = class XMLHttpRequest extends window.XMLHttpRequest {
     this.addEventListener('timeout', () => {});
   }
 }
-
-// 方案2
-const OriginXMLHttpRequest = window.XMLHttpRequest;
-
-window.XMLHttpRequest = function XMLHttpRequest(...rest) {
-  OriginXMLHttpRequest.apply(this, rest);
-  this.addEventListener('loadstart', () => {});
-  this.addEventListener('loadend', () => {});
-  this.addEventListener('load', () => {});
-  this.addEventListener('abort', () => {});
-  this.addEventListener('error', (ev) => {
-    console.log(ev);
-  });
-  this.addEventListener('timeout', () => {});
-};
-
-Reflect.setPrototypeOf(XMLHttpRequest, Object.create(OriginXMLHttpRequest.prototype));
-XMLHttpRequest.prototype.construct = XMLHttpRequest;
 
 // 覆写 fetch
 window.fetch = async (...rest) => {
